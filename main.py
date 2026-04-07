7b7654d753aa18bdfdbca79a77c153a0e9bcefbf
israelamare2-cell <israelamare2@gmail.com>
2026-04-07 22:26:38 +0300

    main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if GEMINI_API_KEY:
     genai.configure(api_key=GEMINI_API_KEY)
 
 # በአዲሱ ሞዴል ስሪት ተተክቷል
-model = genai.GenerativeModel('gemini 2.0-Flash-Lite') 
+model = genai.GenerativeModel('gemini-2.5-flash') 
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 app = Flask(__name__)
